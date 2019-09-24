1e41b29b5ea5f610d1ce193f054c6f5b2e34ae85
Alexey Galmanov <alex.galmanov@gmail.com>
2019-09-25 01:06:53 +0300

    log cteate table

--- packages/gql-server/log/lib/create_table.js
+++ packages/gql-server/log/lib/create_table.js
@@ -1,1 +1,8 @@
-module.exports = ({ color, show }) => {}
+const { createTimestamp, getLabel, getLevel } = require('./segments')
+
+module.exports = ({ tags, filename }, { color, show }, log) => row => {
+    if (!show) return
+    if (!row || !Array.isArray(row)) return log.error(new Error('ROW must be an array'))
+    console.log(createTimestamp(), getLabel(tags, filename), getLevel('table', color))
+    console.table(row)
+}
