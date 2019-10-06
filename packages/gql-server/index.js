f62269751d0e8fa43f6de49a4bab9cc682560a82
Alexey Galmanov <alex.galmanov@gmail.com>
2019-10-06 21:40:13 +0300

    gql-server install @pw/utils/express

--- packages/gql-server/index.js
+++ packages/gql-server/index.js
@@ -1,12 +1,19 @@
 require('./config')
-
 const log = require('@pw/log')(module)
-const app = require('./app')
+const { createApp } = require('@pw/express')
+const path = require('path')
 const routes = require('./routes')
 const errorMdw = require('./mdw/error')
 const notFoundErrorMdw = require('./mdw/not_found')
 
 const { GQL_SERVER_PORT } = process.env
+
+const app = createApp({
+    logger: 'dev',
+    favicon: path.join(__dirname, 'root', 'favicon.ico'),
+    views: path.join(__dirname, 'views'),
+    static: path.join(__dirname, 'root'),
+})
 
 app.use(routes)
 // routes(app)
