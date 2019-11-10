49cc9c731f791551af0102767ec058733f43724a
Alexey Galmanov <alex.galmanov@gmail.com>
2019-11-10 13:55:49 +0200

    setWebcam validate

--- utils/validate/webcam.js
+++ utils/validate/webcam.js
@@ -1,8 +1,24 @@
 const yup = require('yup')
 
 module.exports = yup.object().shape({
-    input: yup
+    title: yup
         .string()
-        .strict(true)
+        .min(2)
+        .max(20)
         .required(),
+    point: yup.object().shape({
+        lat: yup.number().required(),
+        lng: yup.number().required(),
+    }),
+    origin: yup.object().shape({
+        url: yup
+            .string()
+            .url()
+            .required(),
+        stream: yup
+            .string()
+            .url()
+            .required(),
+        title: yup.string(),
+    }),
 })
