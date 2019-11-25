23b667db66be0464a65e9d0b2ae6f42e191aff36
nerjs <karifan.stap@gmail.com>
2019-11-25 05:27:37 +0200

    add useRoutePosition

---
+++ packages/admin/src/hooks/useRoutePosition.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState, useCallback } from 'react'
+import MapContainer from './container'
+import Map from '@comp/map/index'
+import { useParams, useHistory, useLocation, useRouteMatch } from 'react-router-dom'
+
+export default () => {
+    const location = useLocation()
+    const history = useHistory()
+    const { pos } = useParams()
+
+    let mounted = false
+
+    useEffect(() => {
+        mounted = true
+        return () => {
+            mounted = false
+        }
+    })
+
+    const changePos = useCallback(({ center, zoom }) => {
+        if (!mounted) return
+        const newPos = `${center[0]}:${center[1]}:${zoom}`
+        let newPath = ''
+        if (pos) {
+            newPath = location.pathname.replace(pos, newPos)
+        } else {
+            newPath = `${location.pathname.replace(/(\/)$/, '')}/${newPos}`
+        }
+
+        history.replace(newPath)
+    }, [])
+
+    if (!pos) return { changePos }
+
+    const [lat, lng, zoom] = pos.split(':').map(n => Number(n))
+
+    return {
+        center: !isNaN(lat) && !isNaN(lng) ? [lat, lng] : undefined,
+        zoom: !isNaN(zoom) && zoom > 1 && zoom < 15 ? zoom : undefined,
+        changePos,
+    }
+}
--- packages/admin/src/routes/map/index.js
+++ packages/admin/src/routes/map/index.js
@@ -2,47 +2,10 @@ import React, { useEffect, useState, useCallback } from 'react'
 import MapContainer from './container'
 import Map from '@comp/map/index'
 import { useParams, useHistory, useLocation, useRouteMatch } from 'react-router-dom'
-
-const getPos = () => {
-    const location = useLocation()
-    const history = useHistory()
-    const { pos } = useParams()
-
-    let mounted = false
-
-    useEffect(() => {
-        mounted = true
-        return () => {
-            mounted = false
-        }
-    })
-
-    const changePos = useCallback(({ center, zoom }) => {
-        if (!mounted) return
-        const newPos = `${center[0]}:${center[1]}:${zoom}`
-        let newPath = ''
-        if (pos) {
-            newPath = location.pathname.replace(pos, newPos)
-        } else {
-            newPath = `${location.pathname.replace(/(\/)$/, '')}/${newPos}`
-        }
-
-        history.replace(newPath)
-    }, [])
-
-    if (!pos) return { changePos }
-
-    const [lat, lng, zoom] = pos.split(':').map(n => Number(n))
-
-    return {
-        center: !isNaN(lat) && !isNaN(lng) ? [lat, lng] : undefined,
-        zoom: !isNaN(zoom) && zoom > 1 && zoom < 15 ? zoom : undefined,
-        changePos,
-    }
-}
+import useRoutePosition from '../../hooks/useRoutePosition'
 
 const MapRoute = () => {
-    const { center, zoom, changePos } = getPos()
+    const { center, zoom, changePos } = useRoutePosition()
 
     return (
         <MapContainer>
