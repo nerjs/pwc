832dfde496f9e184f572a3791c0eb4453fab4530
nerjs <karifan.stap@gmail.com>
2019-12-01 15:06:43 +0200

    success ad webcam

--- packages/admin/src/components/formAdd/index.js
+++ packages/admin/src/components/formAdd/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Formik, Form } from 'formik'
 import { useMutation } from '@apollo/react-hooks'
+import { useHistory } from 'react-router-dom'
 import webcamInputSchema from '@pw/validate/webcam_input'
 import AddForm from './form'
 import SubmitBtn from './btn'
@@ -22,22 +23,30 @@ const initialValues = {
 
 const AddPointForm = () => {
     const [addWebcam] = useMutation(addWebcamGql, { errorPolicy: 'all' })
-    const handleSubmit = async (values, { setFieldError, setSubmitting, setStatus }) => {
+    const history = useHistory()
+
+    const handleSubmit = async (values, { setFieldError, setSubmitting, setStatus, resetForm }) => {
         setSubmitting(true)
         try {
-            const result = await addWebcam({
+            const {
+                data: {
+                    addWebcam: { id, point },
+                },
+            } = await addWebcam({
                 variables: { input: values },
             })
 
-            console.log('result', result)
+            resetForm()
+
+            history.push(`/map/${point.lat}:${point.lng}:12/${id}`)
         } catch (e) {
             const errors = parseValidationError(e, 'addWebcam')
 
             if (!errors) return setStatus(e.message)
 
             errors.forEach(({ path, message }) => setFieldError(path, message))
+            setSubmitting(false)
         }
-        setSubmitting(false)
     }
 
     return (
