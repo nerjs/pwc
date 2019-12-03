147083e6cec5c8acecc032d7cae0af4b23322b91
nerjs <karifan.stap@gmail.com>
2019-12-03 06:27:32 +0200

    origin info block

--- packages/admin/src/components/formAdd/info/origin.js
+++ packages/admin/src/components/formAdd/info/origin.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import useDebounce from 'react-use/lib/useDebounce'
+import Microlink from '@microlink/react'
 
-const OriginInfoBlock = () => {
-    return null
+const Title = styled.h2`
+    font-size: 20px;
+    display: block;
+    text-align: center;
+`
+
+const Hr = styled.hr`
+    width: 90%;
+    margin: 10px auto;
+`
+
+const Link = styled.a`
+    font-size: 18px;
+    text-decoration: none;
+    padding-left: 10px;
+    color: #406805;
+`
+
+const Preview = styled(Microlink)`
+    width: 100%;
+    margin: 10px auto;
+`
+
+const OriginInfoBlock = ({ value, error }) => {
+    const { url, title } = value || {}
+    const urlError = error && error.url
+    const [currentLink, setCurrentLink] = useState(null)
+
+    useDebounce(
+        () => {
+            if (urlError || !url || url.length === 1) return
+            setCurrentLink(url)
+        },
+        100,
+        [url, urlError, setCurrentLink],
+    )
+
+    return (
+        <>
+            <Title>{title}</Title>
+            <Hr />
+            {currentLink && (
+                <>
+                    <Link href={currentLink} target="_blank">
+                        {currentLink}
+                    </Link>
+                    <Hr />
+                    <Preview url={currentLink} size="large" />
+                </>
+            )}
+        </>
+    )
 }
 
 export default OriginInfoBlock
