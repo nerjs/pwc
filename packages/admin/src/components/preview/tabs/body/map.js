d338503d772c5f79a506ccc04309f21b1ff7dc17
nerjs <karifan.stap@gmail.com>
2019-12-20 17:18:31 +0200

    tabs body map adress

--- packages/admin/src/components/preview/tabs/body/map.js
+++ packages/admin/src/components/preview/tabs/body/map.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
 import propTypes from 'prop-types'
+import { SpinIcon } from '@comp/icon'
+import { MapProvider, useGeocoder } from '../../../map'
 
 const TabBodyMapContainer = styled.div``
 
+const StyledTabBodyMapAdress = styled.div``
+
+const TabBodyMapPlace = styled.div``
+
+const TabBodyMapAdress = ({ point }) => {
+    const [loading, codes] = useGeocoder(point)
+
+    const adress =
+        codes && codes.length > 0 ? codes.find(({ types }) => types.indexOf('route') < 0) : ''
+
+    return (
+        <StyledTabBodyMapAdress>
+            {loading ? <SpinIcon /> : adress && adress.formatted_address}
+        </StyledTabBodyMapAdress>
+    )
+}
+
 const TabBodyMap = ({ point: { lat, lng } }) => {
-    return <TabBodyMapContainer>map tab</TabBodyMapContainer>
+    return (
+        <TabBodyMapContainer>
+            <MapProvider>
+                <TabBodyMapAdress point={{ lat, lng }} />
+                <TabBodyMapPlace></TabBodyMapPlace>
+            </MapProvider>
+        </TabBodyMapContainer>
+    )
 }
 
 TabBodyMap.propTypes = {
